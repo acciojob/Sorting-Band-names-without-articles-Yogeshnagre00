60b1f5fb35fd892ff5ae893c4f902e41420972e9
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-13 18:59:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-//your code here
-let touristSpots = ['The Virupaksha Temple', 'an Victoria Memorial', 'a xTajmahal'];
+const bandNames = ['The Beatles', 'Red Hot Chili Peppers', 'Metallica', 'Aerosmith', 'The Rolling Stones', 'Nirvana'];
 
-function tri(name){
-	 return name.replace(/^(a |the |an )/i,'');
-  
- }
+// Function to remove articles from the band names
+const removeArticles = (name) => {
+  // List of articles to be removed
+  const articles = ['the', 'a', 'an'];
 
- let a= touristSpots.sort((a,b) => tri(a)>tri(b) ? 1 : -1 );
- console.log(a);
+  // Convert the name to lowercase
+  const lowercaseName = name.toLowerCase();
 
+  // Check if the name starts with an article
+  for (let article of articles) {
+    if (lowercaseName.startsWith(article + ' ')) {
+      // Remove the article from the name
+      return name.substring(article.length + 1);
+    }
+  }
 
+  return name;
+};
+
+// Remove articles and sort the band names
+const sortedBandNames = bandNames.map(removeArticles).sort();
+
+// Print the sorted band names
+console.log(sortedBandNames);
