f738fb695841c811c77d5aa66bf524918b5f7e23
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-13 18:55:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here
+function stripArticles(bandName) {
+  // Create a regular expression to match articles
+  const regex = new RegExp('^(a |the |an )', 'i');
 
+  // Replace any articles in the band name with an empty string
+  return bandName.replace(regex, '');
+}
+
+// Get the array of band names
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+// Sort the band names without articles
+const sortedBands = bands.map(stripArticles).sort();
+
+// Create a list element for each band name
+const bandList = sortedBands.map((bandName) => `<li>${bandName}</li>`);
+
+// Append the list of band names to the band ul element
+document.getElementById('band').innerHTML = bandList.join('');
